280b6a7d2bb4e1f96fadd7266c795440043173a5
Ale-1407 <alessiacarrera07@gmail.com>
2022-10-25 17:00:59 +0200

    completato snack-1

--- snack-1/js/main.js
+++ snack-1/js/main.js
@@ -5,3 +5,20 @@ stampa il numero successivo ( del numero inserito dall'utente:
 esempio se il numero dell'utente è 2, il numero successivo è 3).
 */
 
+
+let number = '';
+
+//faccio scegliere all'utente un numero da 1 a 10
+//solo un ciclo => i <= 1;
+
+for (let i = 1; i <= 1; i++){
+
+  let number = parseInt(prompt ('Inserisci un numero da 1 a 10'));
+
+    if (number % 2 == 0){
+        document.getElementById('risPari').innerText = `${number} è un numero PARI`
+    } else{
+        numSucc = number + 1;
+        document.getElementById('risDisp').innerText = `Hai inserito un numero dispari, questo è il tuo nuovo numero: ${numSucc}`
+    }
+}
